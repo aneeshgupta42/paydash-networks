7ce45e9877148292a63c7b7c0c443f7e4fc431f8
aneesh.gupta <aneeshgupta8@gmail.com>
2019-08-02 17:27:18 +0530

    resolving conflict in exact_matches (location matching)

--- case_specific_matches_code_separator/helpers/exact_matches.py
+++ case_specific_matches_code_separator/helpers/exact_matches.py
@@ -20,17 +20,6 @@ def standardize_string(s):
 def check_exact_match(df_networks, df_blocks):
 
 	def set_predictions(row, df_blocks):
-<<<<<<< HEAD:case_specific_matches_code_separator/helpers/exact_matches.py
-		
-=======
-		if row['exact_match_blocks'] == 1:
-			row['block_prediction'] = row['Location']
-			row['block_prediction_score'] = 100
-			row['district_prediction'] = \
-				(df_blocks.loc[df_blocks['block_name'] == row['Location_std'], 'district_name']).values[0]
-			row['district_prediction_score'] = 100
-
->>>>>>> 644a6610c7e06b83a3daafab7ce3f32a7814ae55:archive/case_specific_matches_code_separator/helpers/exact_matches.py
 		if row['exact_match_full_name'] == 1:
 			row['block_prediction'] = \
 				(df_blocks.loc[df_blocks['full_name_std'] == row['Location_std'], 'block_name']).values[0]
@@ -38,16 +27,16 @@ def check_exact_match(df_networks, df_blocks):
 
 			row['district_prediction'] = \
 				(df_blocks.loc[df_blocks['full_name_std'] == row['Location_std'], 'district_name']).values[0]
-			row['district_prediction_score'] = 100	
+			row['district_prediction_score'] = 100
 
 
 		elif row['exact_match_blocks'] == 1:
 			# take out comma for "SEONI, MALWA"
 			row['block_prediction'] = row['Location'].replace(",", "")
 			row['block_prediction_score'] = 100
-			
+
 			if row['exact_match_districts'] == 1:
-				# block name is also a district name 
+				# block name is also a district name
 				# get corresponding district of matched block
 				row['district_prediction'] = \
 					(df_blocks.loc[df_blocks['block_name'] == row['Location_std'], 'district_name']).values[0]
@@ -95,25 +84,14 @@ def process_exact_matches(df_networks, df_blocks):
 		df_blocks['full_name'].apply(standardize_string)
 
 	df_networks = check_exact_match(df_networks, df_blocks)
-<<<<<<< HEAD:case_specific_matches_code_separator/helpers/exact_matches.py
-	
-	df_networks = df_networks[['Res_uid', 'Name', 'Designation', 
-							   'Location', 'Location_std', 
-							   'block_prediction', 'block_prediction_score', 
-							   'district_prediction', 
-							   'district_prediction_score', 
-							   'exact_match_blocks', 'exact_match_districts',
-							   'exact_match_full_name']]
-=======
 
 	df_networks = df_networks[['Res_uid', 'Name', 'Designation',
 							   'Location', 'Location_std',
 							   'block_prediction', 'block_prediction_score',
 							   'district_prediction',
 							   'district_prediction_score',
-							   'exact_match_blocks', 'exact_match_full_name']]
->>>>>>> 644a6610c7e06b83a3daafab7ce3f32a7814ae55:archive/case_specific_matches_code_separator/helpers/exact_matches.py
-
+							   'exact_match_blocks', 'exact_match_districts',
+							   'exact_match_full_name']]
 	print(df_networks)
 	print(df_blocks)
 	return df_networks, df_blocks
